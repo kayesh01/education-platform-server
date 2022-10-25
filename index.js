61c94c9ec183b81b7a686923d920115a1e8707c7
kayesh01 <shaanfarhan01@gmail.com>
2022-10-25 13:35:23 +0600

    categories data loaded

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('Learning website is runing')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port, () => {
