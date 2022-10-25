6c9bf31ba1b597228e721c2c5dc0f747a3736338
kayesh01 <shaanfarhan01@gmail.com>
2022-10-25 13:22:43 +0600

    categories.json added

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('Learning website is runing')
 })
+
+app.listen(port, () => {
+    console.log('Education learning site running on port', port);
+})
