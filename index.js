743fd8e57772db062341c0973d1df1d1d74ccebb
kayesh01 <shaanfarhan01@gmail.com>
2022-10-28 12:10:29 +0600

    vrecel done

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 
-const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Learning website is runing')
 })
 
-app.get('/categories', (req, res) => {
-    res.send(categories);
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(n => n.id === id);
+    res.send(course);
 })
 
 app.listen(port, () => {
